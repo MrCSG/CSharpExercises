3169f5a857e41b1ceb0ddf863d1064253a13b2fe
SebasAKD <racingymotos@gmail.com>
2019-05-09 21:56:23 -0300

    Todos los ejercicios realizados de la clase 3.

--- 3.TiposDatos/DataTypeExercises.cs
+++ 3.TiposDatos/DataTypeExercises.cs
@@ -3,13 +3,31 @@ using System;
 namespace CSharpExercises{
     public class DataTypeExercises{
         public void ValueSum(){
+            int firstValue=0, secondValue=0;
+            bool canParseValue;
             Console.WriteLine("Ingrese un valor a sumar:");
-            var firstValue = Console.ReadLine();
-
-            Console.WriteLine("Ingrese otro valor a sumar:");
-            var secondValue = Console.ReadLine();
-
-            Console.WriteLine("El resultado de la suma es: {0}", firstValue + secondValue);
+            //var firstValue = Console.ReadLine();
+            //int firstValue = Convert.ToInt32(Console.ReadLine());
+            canParseValue = Int32.TryParse(Console.ReadLine(),out firstValue);
+            if (canParseValue)
+            {
+                Console.WriteLine("Ingrese otro valor a sumar:");
+                //var secondValue = Console.ReadLine();
+                //int secondValue = Int32.Parse(Console.ReadLine());
+                canParseValue = Int32.TryParse(Console.ReadLine(),out secondValue);
+                if(canParseValue)
+                {
+                    Console.WriteLine("El resultado de la suma es: {0}", firstValue + secondValue);
+                }
+                else
+                {
+                    Console.WriteLine("El segundo valor no es un número.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El primer valor no es un número.");
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,19 +6,30 @@ namespace CSharpExercises
     {
         static void Main(string[] args)
         {
-            /*//1.
+            Console.Write("Ingrese la opción que desea realizar (1,2 o 3): ");
+            string choosedOption = Console.ReadLine();
+            if(choosedOption=="1")
+            {
+            //1.
             ConsoleExercises console = new ConsoleExercises();
-            console.AskAge();*/
+            console.AskAge();
 
             //Pedir nombre, apellido y documento e imprimir toda la información al final.
-
+            } else if(choosedOption=="2")
+            {
             //2.
             VariableExercises variable = new VariableExercises();
             variable.Concatenador();
-            
-            /* //3.
+            } else if (choosedOption=="3")
+            {            
+            //3.
             DataTypeExercises dataType = new DataTypeExercises();
-            dataType.ValueSum();*/
+            dataType.ValueSum();
+            }
+            else
+            {
+                Console.WriteLine("La opción ingresada no existe.");
+            }
         }
     }
 }
