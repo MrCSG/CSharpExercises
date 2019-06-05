6eb13d34e3f15f0bde9794e717f08fea66440ec0
Juan Agustin Gimenez <juang0627@alu.bue.edu.ar>
2019-06-05 18:47:52 -0300

    Ejercicio 9 realizado solo con while

--- 9.CyclicRotation/Solution.cs
+++ 9.CyclicRotation/Solution.cs
@@ -5,8 +5,27 @@ namespace _9.CyclicRotation
     public class Solution
     {
         public int[] solution(int[] A, int K){
-            int swap;
-            while(K>0)
+            int swap=0, i=A.Length-1;
+            while(K>0 && i>=0)
+            {
+                if(i == A.Length-1)
+                {
+                    swap = A[A.Length-1];
+                    i--;
+                }else
+                {
+                    A[i+1] = A[i];
+                    i--;
+                }
+                if(i == 0)
+                {
+                    A[i+1] = A[i];
+                    A[0] = swap;
+                    K--;
+                    i = A.Length-1;
+                }
+            }
+            /*while(K>0)
             {
                 swap = A[A.Length-1];
                 for(int i=A.Length-2; i>=0 ; i--)
@@ -15,9 +34,9 @@ namespace _9.CyclicRotation
                 }
                 A[0] = swap;
                 K--;
-            }
+            }*/
 
-            for(int i=0 ; i<A.Length; i++)
+            for(i=0 ; i<A.Length; i++)
             {
                 Console.WriteLine(A[i]);
             }
